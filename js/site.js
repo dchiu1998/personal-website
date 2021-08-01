76f659ff7f600bc58fb98f26c78fce19d8bcb325
dchiu1998 <44936461+dchiu1998@users.noreply.github.com>
2021-08-01 01:54:55 -0400

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,15 @@
+var coll = document.getElementsByClassName("slide-down");
+
+for (var i = 0; i < coll.length; i++){
+    coll[i].addEventListener("click", slide);
+}
+
+function slide(){
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+}
